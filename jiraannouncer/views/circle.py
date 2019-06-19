3c797a9cc0d50b8948a76a96359f52e1edd58435
Eearslya Sleiarion <eearslya@gmail.com>
2019-06-19 08:42:11 -0700

    Slight formatting change
    Fix the duplicate "reponame/reponame" to "username/reponame" and add a closing paren.

--- jiraannouncer/views/circle.py
+++ jiraannouncer/views/circle.py
@@ -43,10 +43,10 @@ def circle(request):
     else:
         compareurl = data['compare']
     message1 = f"""
-                [\x0315CircleCI\x03] \x0306 {data['reponame'] or ''}/{data['reponame'] or ''}
+                [\x0315CircleCI\x03] \x0306 {data['username'] or ''}/{data['reponame'] or ''}
                  \x03#{data['build_num'] or ''} (\x0306{data['branch'] or ''}\x03 -  
                  {data['vcs_revision'][:7] or ''} : \x0314 {data['user']['login'] or ''}
-                 \x03: {data['outcome'] or ''}
+                 \x03: {data['outcome'] or ''})
                 """
     message2 = f"""
                 [\x0315CircleCI\x03] Change view: \x02\x0311{compareurl}

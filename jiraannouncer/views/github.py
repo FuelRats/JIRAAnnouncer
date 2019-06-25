9af7dd3cd4936d8f9a33a8eb7b061485f5d84135
Kenneth Aalberg <kenneaal@gmail.com>
2019-06-25 18:46:25 +0200

    Session->dbsession

--- jiraannouncer/views/github.py
+++ jiraannouncer/views/github.py
@@ -234,7 +234,7 @@ def github(prequest):
         oldrecords = request.session.query(githubmodels.GitHubMessage).filter_by(timestamp < timeout).all()
         if oldrecords.count() > 0:
             logprint(f"Deleted {oldrecords.count} old GitHub messages.")
-            request.session.delete(oldrecords)
+            request.dbsession.delete(oldrecords)
     if lastmessage['full'] == message:
         logprint("Duplicate message, skipping:")
         logprint(message)

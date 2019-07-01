75af1bf5be6b6f111ffb230dc68a9afac8200462
Kenneth Aalberg <kenneaal@gmail.com>
2019-07-01 21:28:29 +0200

    Add basic TeamCity announcements.

--- jiraannouncer/views/teamcity.py
+++ jiraannouncer/views/teamcity.py
@@ -5,6 +5,16 @@ from pyramid.view import view_config
 
 from ..utils import logprint, jsondump, send, getlast, demarkdown, devsay
 
+notifyTypes = {
+    'buildStarted': '\x0307Build Started\x03',
+    'buildInterrupted': '\x03\x02Build Interrupted\x03',
+    'buildFinished': '\x0303Build Finished\x03'
+}
+buildresults = {
+    'running': '\x0308Running\x03',
+    'failure': '\x02\x0304Failure\x03',
+    'successful': '\x0303Successful\x03'
+}
 
 @view_config(route_name='teamcity', renderer='json')
 def teamcity(request):
@@ -21,6 +31,6 @@ def teamcity(request):
     else:
         build = {}
         logprint("No buildinfo in TeamCity call!")
-    message = f"[TeamCity] {build['projectName']} - {build['notifyType']}: {build['text']}"
+    message = f"\x0303[TeamCity]\x03 {build['projectName']} - {notifyTypes[build['notifyType']]}: {build['text']}"
     send("#rattech", message, '')
     return

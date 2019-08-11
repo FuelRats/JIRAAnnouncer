652ad1ea6b8d8bb00f0a04cfb1127886a24e1df6
Kenneth Aalberg <kenneaal@gmail.com>
2019-08-11 18:47:35 +0200

    Add Prometheus datapoints to client endpoint.

--- jiraannouncer/views/client.py
+++ jiraannouncer/views/client.py
@@ -5,8 +5,15 @@ from sys import hexversion
 
 from ..utils import send, devsay
 import logging
+from prometheus_client import Counter
+
 
 log = logging.getLogger(__name__)
+announcer_client_requests = Counter('announcer_client_requests', 'Cumulative calls to Client announcer')
+announcer_client_unsigned = Counter('announcer_client_unsigned', 'Unsigned calls to Client announcer')
+announcer_client_noref = Counter('announcer_client_noref', 'Unsigned calls with no or invalid referer')
+announcer_client_signed = Counter('announcer_client_signed', 'Signed calls to Client announcer')
+announcer_client_signfail = Counter('announcer_client_failed', 'Calls to announcer failing HMAC check')
 
 
 @view_config(route_name='client', renderer="json")
@@ -14,6 +21,7 @@ def client(request):
     """Handle Client arrival announcements."""
     referer = request.headers['Referer'] if 'referer' in request.headers else None
     possiblefake = False
+    announcer_client_requests.inc()
 
     if 'X-Client-Signature' in request.headers:
         settings = request.registry.settings
@@ -29,6 +37,7 @@ def client(request):
         if hexversion >= 0x020707F0:
             if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
                 log.error("Signature mismatch, possible fake call!")
+                announcer_client_signfail.inc()
                 possiblefake = True
         else:
             if not str(mac.hexdigest()) == str(signature):
@@ -36,12 +45,15 @@ def client(request):
                 log.error(f"{mac.hexdigest()} vs {str(signature)}")
                 devsay(f"Invalid MAC in Client message: {str(signature)}")
                 possiblefake = True
+                announcer_client_signfail.inc()
     elif referer != "https://clients.fuelrats.com:7778/":
         log.error(f"Client announcer called with invalid referer: {referer}")
         devsay(f"Someone tried to call the client announcer with an invalid referer '{referer}'! Absolver!")
         possiblefake = True
+        announcer_client_noref.inc()
     else:
         log.warn("Non-signed request from valid referer.")
+        announcer_client_unsigned.inc()
     try:
         cmdrname = request.params['cmdrname']
         system = request.params['system']
@@ -50,7 +62,6 @@ def client(request):
     except NameError:
         log.critical("Missing parameters to Client announcement call.")
         devsay("Parameters were missing in a Client announcement call!")
-
     if 'extradata' not in request.params:
         message = f"Incoming Client: {cmdrname} - System: {system} - Platform: {platform} - O2: {o2status}"
     else:

11d493947eacec47bf9cce7039fa7d834fc1ef24
Kenneth Aalberg <kenneaal@gmail.com>
2019-09-27 21:10:13 +0200

    Separate template. Remove strange validator erroring.

--- jiraannouncer/views/drill.py
+++ jiraannouncer/views/drill.py
@@ -1,17 +1,17 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 import colander
-from deform import Form, ValidationFailure
+from deform import Form, ValidationFailure, widget
 
 
 class Drill(colander.MappingSchema):
     clientname = colander.SchemaNode(colander.String())
     system = colander.SchemaNode(colander.String())
-    platform = colander.SchemaNode(colander.String(), validator=colander.OneOf('PC', 'PS', 'XB'))
+    platform = colander.SchemaNode(colander.String())
     overseer = colander.SchemaNode(colander.String())
 
 
-@view_config(route_name='drill', renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='drill', renderer='../templates/form.jinja2')
 def my_view(request):
     schema = Drill()
     drillform = Form(schema, buttons=('submit',))
@@ -23,10 +23,11 @@ def my_view(request):
             appstruct = drillform.validate(controls)
         except ValidationFailure as e:
             return {'form': e.render()}
-        return {'form': None, 'appstruct':appstruct}
+        return {'form': None, 'appstruct': appstruct}
     else:
         try:
             schema = Drill()
 
         except:
             return
+    return dict(form=drillform)

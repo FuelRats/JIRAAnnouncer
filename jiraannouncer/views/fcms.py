05c6be8ab5bb7746c345a34db20f4db48f3caad2
Chris Gårdenberg <webmaster@itssimple.se>
2020-06-14 21:45:13 +0200

    feat: Added carrier vanity name to output

--- jiraannouncer/views/fcms.py
+++ jiraannouncer/views/fcms.py
@@ -2,7 +2,7 @@ import time
 
 from pyramid.view import view_config
 
-from ..utils import send, devsay
+from ..utils import send, devsay, from_hex
 import logging
 
 log = logging.getLogger(__name__)
@@ -20,14 +20,15 @@ def fcms(request):
         return
 
     carrier = data['carrier_callsign']
+    carrier_name = from_hex(data['carrier_vanity_name'])
     target_system = data['calendar_arrivalSystem']
     departure_system = data['current_starsystem']
     jump_time = data['calendar_end'] if 'calendar_end' in data else None
 
     if jump_time is None:
-        message = f"[\x0315FCMS\x03] \x02Carrier {carrier}\x02 cancelled their scheduled jump"
+        message = f"[\x0315FCMS\x03] \x02{carrier_name} ({carrier})\x02 cancelled their scheduled jump"
     else:
-        message = f"[\x0315FCMS\x03] \x02Carrier {carrier}\x02 is will travel from \x02{departure_system}\x02 to \x02{target_system}\x02 at \x02{jump_time}\x02"
+        message = f"[\x0315FCMS\x03] \x02{carrier_name} ({carrier})\x02 is will travel from \x02{departure_system}\x02 to \x02{target_system}\x02 at \x02{jump_time}\x02"
 
     msgshort = {"time": time.time(), "type": "FCMS",
                 "key": "FCMS", "full": message}

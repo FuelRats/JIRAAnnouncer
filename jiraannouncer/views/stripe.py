3d8abf4ef29d0786ed191d395c18d2634adc97df
Kenneth Aalberg <kenneaal@gmail.com>
2020-12-27 14:46:39 +0100

    Fix ANSI.

--- jiraannouncer/views/stripe.py
+++ jiraannouncer/views/stripe.py
@@ -24,8 +24,10 @@ def mystripe(request):
         amount = float(f'{str(payment_intent.amount)[:decimalplace]}.{str(payment_intent.amount)[decimalplace:]}')
         if payment_intent.currency=='usd':
             numsnickers = str(round(amount / 1.48))
-        elif  payment_intent.currency == 'eur':
+        elif payment_intent.currency == 'eur':
             numsnickers = str(round(amount / 1.25))
+        elif payment_intent.currency == 'gbp':
+            numsnickers = str(round(amount / 0.65))
         else:
             numsnickers = 'an unknown amount of'
         print(f'[\x0315Stripe\x03] A donation of \x0315{str(amount)}\x03 {payment_intent.currency.upper()} '
